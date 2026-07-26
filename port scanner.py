bdf7b4602264ca688f344ec34c66dd9dfc11c726
AlpachaAlikhan <nariman.gusseinov277nariman@gmail.com>
2026-07-26 09:15:43 +0500

    Updated get_ip function, now it is using ipaddress library

--- port scanner.py
+++ port scanner.py
@@ -9,14 +9,15 @@ def main():
     pass
 
 def get_ip():
-    ip = input("Enter the IP address to scan: ")
-    if ip.count('.') != 3:
-        print("Invalid IP address format. Please enter a valid IPv4 address.")
-        return get_ip()
-    elif not all(0 <= int(part) < 256 for part in ip.split('.')):
-        print("Invalid IP address. Each octet must be between 0 and 255.")
-        return get_ip()
-    return ip
+    while True:
+        ip = input("Enter the IP address to scan: ")
+
+        try:
+            ipaddress.ip_address(ip)
+            return ip
+        except ValueError:
+            print("Invalid IP address")
+        
 
 def get_portrange():
     port_range = input("Enter the port range to scan (e.g., 20-80): ")

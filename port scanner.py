8e3bd068ba0f93f8e671b4afaaf6a7c8bed44714
AlpachaAlikhan <nariman.gusseinov277nariman@gmail.com>
2026-07-26 09:52:30 +0500

    First working version of scanner

--- port scanner.py
+++ port scanner.py
@@ -1,13 +1,16 @@
 import socket
 import ipaddress
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-
-
 def main():
-    pass
-
+    ip = get_ip()
+    port_range = get_port_range()
+    results = []
+    for port in range(port_range):
+        if scan_port(ip, port):
+            results.append(port)
+    print_results(results)
+    
+        
 def get_ip():
     while True:
         ip = input("Enter the IP address to scan: ")
@@ -19,19 +22,32 @@ def get_ip():
             print("Invalid IP address")
         
 
-def get_portrange():
-    port_range = input("Enter the port range to scan (e.g., 20-80): ")
-    try:
-        start_port, end_port = map(int, port_range.split('-'))
-        if start_port < 0 or end_port > 65535 or start_port > end_port:
-            raise ValueError
-    except ValueError:
-        print("Invalid port range. Please enter a valid range (e.g., 20-80).")
-        return get_portrange()
-    return start_port, end_port
+def get_port_range():
+    while True:
+        port_range = input("Enter the port range to scan (e.g., 20-80): ")
+        try:
+            start_port, end_port = map(int, port_range.split('-'))
+            if start_port < 1 or end_port > 65535 or start_port > end_port:
+                raise ValueError
+            return start_port, end_port
+            
+        except ValueError:
+            print("Invalid port range. Please enter a valid range (e.g., 20-80).")
 
-def scan_ports(ip, port_range):
-    pass
+def scan_port(ip, port):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.settimeout(1)
+            return True 
+    except socket.error:
+        return False
+
 
 def print_results(results):
-    pass
+    print("========\nOpen ports\n\n")
+    for result in results:
+        print(results) 
+    print(f"Total: {len(results)}\n========")
+    
+
+scan_port("1.1.1.1", 44)
